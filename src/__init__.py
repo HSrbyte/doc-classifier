3addeb7f4ff67353edeee1eb85757ffdccc76e47
Daniel <dhryniewski@luminess.eu>
2024-03-12 20:17:04 +0000

    add image_grid_sample

--- src/__init__.py
+++ src/__init__.py
@@ -7,11 +7,12 @@ from .text_mining.tesseract import (tesseract_osd, tesseract_ocr,
 
 from .utils.misc import extract_tar_gz, save_jsonfile, read_jsonfile
 
-from .visualization.visualize import draw_tesseract_result, plot_image
+from .visualization.visualize import (draw_tesseract_result, plot_image,
+                                      image_grid_sample)
 
 
 __all__ = [
-    "extract_tar_gz", "save_jsonfile", "read_jsonfile", "plot_image",
+    "extract_tar_gz", "save_jsonfile", "read_jsonfile", "plot_image", "image_grid_sample",
     "draw_tesseract_result", "tesseract_ocr", "tesseract_osd", "image_rotate",
     "tesseract_ocr_postprocess", "tesseract_image_process", "tesseract_data_process"
 ]
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,24 +1,26 @@
 import cv2
+import random
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from numpy.typing import ArrayLike
-from typing import Tuple, Union, Optional
+
+from typing import Tuple, Union, Optional, List
 
 
-def draw_tesseract_result(data: pd.DataFrame,
-                          image: ArrayLike,
-                          bbox_color: Tuple[int, int, int] = (255,0,0),
-                          bbox_thickness: int = 1
-                          ) -> ArrayLike:
+def draw_tesseract_result(
+    data: pd.DataFrame,
+    image: np.ndarray,
+    bbox_color: Tuple[int, int, int] = (255,0,0),
+    bbox_thickness: int = 1
+    ) -> np.ndarray:
     """Draw bounding boxes and text annotations on the input image based on the
     data provided.
 
     Parameters:
         data (pd.DataFrame): DataFrame containing bounding box information and
             text annotations.
-        image (ArrayLike): Input image on which the bounding boxes and text
+        image (np.ndarray): Input image on which the bounding boxes and text
             will be drawn.
         bbox_color (Tuple[int, int, int], optional): Color of the bounding
             boxes in BGR format. Defaults to (255, 0, 0) which is blue.
@@ -26,7 +28,7 @@ def draw_tesseract_result(data: pd.DataFrame,
             Defaults to 1.
 
     Returns:
-        ArrayLike: Image with bounding boxes and text annotations drawn.
+        np.ndarray: Image with bounding boxes and text annotations drawn.
     """
     img = image.copy()
     if len(img.shape) == 2:
@@ -58,11 +60,13 @@ def draw_tesseract_result(data: pd.DataFrame,
     return new_image
 
 
-def plot_image(image: Union[np.ndarray, str],
-               size: Tuple[int,int],
-               title: Optional[str] = None,
-               axis: bool = False,
-               flip_image_layers: bool = True) -> None:
+def plot_image(
+    image: Union[np.ndarray, str],
+    size: Tuple[int,int],
+    title: Optional[str] = None,
+    axis: bool = False,
+    flip_image_layers: bool = True
+    ) -> None:
     """Plot the input image.
 
     Parameters:
@@ -91,3 +95,132 @@ def plot_image(image: Union[np.ndarray, str],
         plt.title(title)
     plt.axis(axis)
     plt.show()
+
+
+def image_grid_sample(
+    images: Union[List[str], List[np.ndarray]],
+    height: int,
+    width: int,
+    square_size: int = 128,
+    bg_color: Union[Tuple[int, int, int], int] = 125,
+    img_layout: str = "auto",
+    grid: bool = True,
+    grid_color: Union[Tuple[int, int, int], int] = 0,
+    grid_thickness: int = 1,
+    seed: int = -1,
+    ) -> np.ndarray:
+    """Create a sample image by arranging a grid of input images.
+
+    Parameters:
+        images (Union[List[str], List[np.ndarray]]): List of input images as
+            file paths or numpy arrays.
+        height (int): Height of the grid (number of rows).
+        width (int): Width of the grid (number of columns).
+        square_size (int, optional): Size of each square in the grid. Defaults
+            to 128 pixels.
+        bg_color (Union[Tuple[int, int, int], int], optional): Background color
+            of the output image. Defaults to 125 (gray).
+        img_layout (str, optional): Layout method for arranging images in the
+            grid. Options are "auto" (automatic layout) or "center" (centered
+            layout). Defaults to "auto".
+        grid (bool, optional): Flag to indicate whether to draw a grid over the
+            output image. Defaults to True.
+        grid_color (Union[Tuple[int, int, int], int], optional): Color of the
+            grid lines. Defaults to 0 (black).
+        grid_thickness (int, optional): Thickness of the grid lines in pixels.
+            Defaults to 1.
+        seed (int, optional): Random seed for shuffling the input images.
+            Defaults to -1 (no seed).
+
+    Returns:
+        np.ndarray: A numpy array representing the generated sample image.
+
+    Notes:
+        - If img_layout is "auto", images will be placed in the grid
+          automatically (on the top-left corner).
+        - If img_layout is "center", images will be centered in each square of
+          the grid.
+        - If the number of input images is greater than the grid size, a random
+          subset of images will be selected for the grid based on the provided
+          seed.
+        - The output image will have a background color specified by bg_color.
+        - If grid is True, a grid will be drawn over the output image with the
+          specified color and thickness.
+    """
+    # check img_layout
+    if img_layout not in ["auto", "center"]:
+        print(f"Not supported method: {img_layout}. Using default method: auto")
+        img_layout = "auto"
+
+    # random sample
+    if len(images) > height * width:
+        if seed == -1:
+            seed = random.randint(0, 9999999)
+        random.seed(seed)
+        images = [images.pop(random.randint(0, len(images)-1)) \
+            for _ in range(height * width)]
+
+    # Output image
+    if isinstance(bg_color, int):
+        bg_color = [bg_color]*3
+    dim = (int(height * square_size), int(width * square_size), 3)
+    img_out = np.full(dim, bg_color, dtype=np.uint8)
+
+    x_offset = 0
+    y_offset = 0
+    for n, img in enumerate(images, start=1):
+        # image read
+        if isinstance(img, str):
+            img = cv2.imread(img, cv2.IMREAD_UNCHANGED)
+        # check layers
+        if len(img.shape) == 2:
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        elif len(img.shape) == 3:
+            if img.shape[2] == 4:
+                img = np.array(img[:, :, :3], dtype=img.dtype)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # image resize
+        h, w = img.shape[:2]
+        r = max(h, w) / square_size
+        dim = (int(w / r), int(h / r))
+        img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+        if img_layout == "auto":
+            img_out[
+                y_offset : y_offset + dim[1],
+                x_offset : x_offset + dim[0]
+                ] = img
+        elif img_layout == "center":
+            cy_offset = (square_size - dim[1]) // 2
+            cx_offset = (square_size - dim[0]) // 2
+            img_out[
+                y_offset + cy_offset : y_offset + cy_offset + dim[1],
+                x_offset + cx_offset : x_offset + cx_offset + dim[0]
+                ] = img
+
+        x_offset += square_size
+        if n % width == 0:
+            x_offset = 0
+            y_offset += square_size
+
+    if not grid:
+        return img_out
+    # Grid
+    if isinstance(grid_color, int):
+        grid_color = [grid_color]*3
+    for i in range(width+1):
+        p1 = [int(i * square_size), 0]
+        p2 = [int(i * square_size), int(square_size * height)]
+        if i == width:
+            p1[0] -= max(1, grid_thickness // 2)
+            p2[0] -= max(1, grid_thickness // 2)
+        cv2.line(img_out, p1, p2, grid_color, grid_thickness)
+    for i in range(height+1):
+        p1 = [0, int(i * square_size)]
+        p2 = [int(square_size * width), int(i * square_size)]
+        if i == height:
+            p1[1] -= max(1, grid_thickness // 2)
+            p2[1] -= max(1, grid_thickness // 2)
+        cv2.line(img_out, p1, p2, grid_color, grid_thickness)
+
+    return img_out
