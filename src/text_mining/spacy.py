85406a03a67d3a563d7db7c0f471cc15397f7d23
Daniel <dhryniewski@luminess.eu>
2024-03-15 11:42:57 +0000

    check spacy models exist

--- src/text_mining/spacy.py
+++ src/text_mining/spacy.py
@@ -1,5 +1,21 @@
 import spacy
 
+def check_and_download_model(model_name: str) -> None:
+    """Check if the specified SpaCy model is installed. If not, download it.
+
+    Parameters:
+        model_name (str): The name of the SpaCy model to check and download.
+
+    Returns:
+        None
+    """
+    if model_name not in spacy.util.get_installed_models():
+        print(f"Downloading the {model_name} model...")
+        try:
+            spacy.cli.download(model_name)
+        except Exception as e:
+            print(f"Error: Unable to download the {model_name} model.\n{e}")
+
 
 def lemmatize_english(words):
     """
@@ -11,6 +27,7 @@ def lemmatize_english(words):
     Returns:
         list: A list of lemmatized words.
     """
+    check_and_download_model("en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
 
     # Lemmatize each word using spaCy French model
@@ -32,6 +49,7 @@ def lemmatize_french(words):
     Returns:
         list: A list of lemmatized words.
     """
+    check_and_download_model("fr_core_news_sm")
     nlp = spacy.load("fr_core_news_sm")
 
     # Lemmatize each word using spaCy French model
