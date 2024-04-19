5ab39ac3ebd68fb96c4617e14cc4f789ca6da92b
Daniel <dhryniewski@luminess.eu>
2024-04-19 16:35:10 +0000

    add text_pipeline function

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,8 @@
-from .dataloader.transformers import image_rotate
+from .dataloader.degradation import image_jpg_compress
+from .dataloader.io import image_read
+from .dataloader.geometric import image_rotate, image_flip
+
+
 
 from .text_mining.nltk import stop_words_filtering
 
@@ -8,16 +12,24 @@ from .text_mining.tesseract import (tesseract_osd, tesseract_ocr, tesseract_ocr_
                                     tesseract_image_process, tesseract_data_process)
 
 from .text_mining.text_process import (extract_document_length, calculate_lexical_diversity,
-                                       calculate_keyword_density)
+                                       calculate_keyword_density, text_pipeline)
+
+from .text_mining.xml import img_xml_unicode, img_xml_language, img_xml_dimension
+
+
 
 from .utils.misc import (extract_tar_gz, save_jsonfile, read_jsonfile,
                          get_all_files_from_data_folder)
+
+
 
 from .visualization.visualize import (draw_tesseract_result, plot_image,
                                       image_grid_sample, create_wordcloud,
                                       barplot)
 
-from .text_mining.xml import img_xml_unicode, img_xml_language, img_xml_dimension
+from .visualization.compute_color_histogram import compute_color_histogram
+
+
 
 from .extract_info.bluriness import bluriness
 
@@ -27,16 +39,17 @@ from .extract_info.img_info import img_info
 
 from .extract_info.extract_image_info import extract_image_info
 
-from .visualization.compute_color_histogram import compute_color_histogram
+
 
 
 
 __all__ = [
     "extract_tar_gz", "save_jsonfile", "read_jsonfile", "plot_image", "image_grid_sample",
-    "draw_tesseract_result", "tesseract_ocr", "tesseract_osd", "image_rotate",
+    "draw_tesseract_result", "tesseract_ocr", "tesseract_osd", "image_rotate", "text_pipeline",
     "tesseract_ocr_postprocess", "tesseract_image_process", "tesseract_data_process",
     "lemmatize_english", "lemmatize_french", "stop_words_filtering", "img_xml_unicode", "img_xml_language",
     "img_xml_dimension", "create_wordcloud", "detect_lang", "get_all_files_from_data_folder",
     "barplot", "bluriness", "gp_name", "extract_image_info", "img_info", "compute_color_histogram",
-    "extract_document_length", "calculate_lexical_diversity", "calculate_keyword_density"
+    "extract_document_length", "calculate_lexical_diversity", "calculate_keyword_density", "image_read",
+    "image_flip", "image_jpg_compress",
 ]
--- src/text_mining/text_process.py
+++ src/text_mining/text_process.py
@@ -1,5 +1,98 @@
+import numpy as np
+from pathlib import Path
+from typing import Union, Tuple, List, Optional
+from nltk.tokenize.regexp import RegexpTokenizer
 
-from typing import List
+from src import (image_read, read_jsonfile, tesseract_image_process, detect_lang,
+                 stop_words_filtering, lemmatize_french, lemmatize_english)
+
+
+def text_pipeline(image: Union[np.ndarray, str, Path],
+                  common_words_data: Union[dict, str, Path],
+                  tokenizer: Optional[RegexpTokenizer] = None,
+                  detect_language: bool = True,
+                  language: str = "english",
+                  keyword_density_lenght: List[int] = [5, 10, 25, 50]
+                  ) -> Tuple[str, List[float]]:
+    """Performs preprocessing on text extracted from an image.
+
+    This function applies several preprocessing steps on the text extracted from an image,
+    including OCR, tokenization, language detection, stop words filtering, lemmatization, and
+    keyword density calculation. The resulting structured text data, along with its features,
+    is returned for further processing or prediction.
+
+    Args:
+        image (Union[np.ndarray, str, Path]): The image from which to extract text.
+            It can be a NumPy array representing the image, a string representing the path to
+            the image file, or a pathlib.Path object.
+        common_words_data (Union[dict, str, Path]): Data containing common words for keyword
+            density calculation. It can be a dictionary, a string representing the path to a
+            JSON file containing the data, or a pathlib.Path object.
+        tokenizer (Optional[RegexpTokenizer], optional): A tokenizer object to use for
+            tokenization. If not provided, a default tokenizer for English and French is used.
+            Defaults to None.
+        detect_language (bool, optional): Whether to detect the language of the text. Defaults
+            to True.
+        language (str, optional): The language to use for preprocessing. If language detection
+            is disabled or inconclusive, this language is used. Defaults to "english".
+        keyword_density_lenght (List[int], optional): List of lengths for common words to use
+            for keyword density calculation. Defaults to [5, 10, 25, 50].
+
+    Returns:
+        Tuple[str, List[float]]: A tuple containing the preprocessed text and its structured
+            features. The text is represented as a string, and the features are represented
+            as a list of floats.
+    """
+    # Check image
+    if isinstance(image, Path):
+        image = str(image)
+    if isinstance(image, str):
+        image = image_read(image)
+
+    # Check common words data
+    if isinstance(common_words_data, Path):
+        common_words_data = str(common_words_data)
+    if isinstance(common_words_data, str):
+        common_words_data = read_jsonfile(common_words_data)
+
+    if tokenizer is None:
+        tokenizer = RegexpTokenizer("[a-zA-ZéèàïôêûçëüÉÈÀÏÔÊÛÇËÜ]{3,}")
+
+    # Tesseract process
+    ocr_result, osd_result = tesseract_image_process(image)
+
+    # Tokenizer
+    words = ' '.join(ocr_result['text'].to_list())
+    tokens = tokenizer.tokenize(words.lower())
+
+    # Language detection
+    if detect_language:
+        lang = detect_lang(tokens)
+        language = 'french' if lang['language'] == 'fr' and lang['score'] > 0.5 else 'english'
+
+    # Stop Words Filtering
+    words_filter = stop_words_filtering(tokens, language)
+
+    # Lemmatize
+    if language == 'french':
+        lemmatized_tokens = lemmatize_french(words_filter)
+    else:
+        lemmatized_tokens = lemmatize_english(words_filter)
+
+    # Words Result
+    text = " ".join(lemmatized_tokens)
+
+    # Structure words data
+    document_lenght = extract_document_length(text)
+    lexical_diversity = calculate_lexical_diversity(text)
+    text_structure = [document_lenght, lexical_diversity]
+    for category, words in common_words_data.items():
+        for k in keyword_density_lenght:
+            words = [w[0] for w in common_words_data[category][:k]]
+            density = calculate_keyword_density(text, words)
+            text_structure.append(density)
+
+    return text, text_structure
 
 
 def extract_document_length(text: str) -> int:
